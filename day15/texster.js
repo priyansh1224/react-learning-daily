5f5f84e7a6e197acf497655c644f5827c74ca523
Priyanshu Oli <priyanshuoli30@gmail.com>
2025-06-26 20:43:01 +0530

    day15 complete

---
+++ day15/texster.js
@@ -0,0 +1,16 @@
+// internal working of store 
+
+const state={
+    slice1:{
+        count:0
+    },
+    slice2:{
+        count:0,
+        name:"rohit"
+
+    },
+    slice2:{
+        count:0,
+        login:true
+    }
+}
